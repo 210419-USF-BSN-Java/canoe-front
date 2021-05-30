0524dc8868271b1548c32eba29e31979911abb96
Tom <noxidwebdev@gmail.com>
2021-05-29 19:35:55 -0700

    refactor signup service --> user service: signup and login (#12)

--- app/src/app/services/user.service.spec.ts
+++ app/src/app/services/user.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { SignUpService } from './sign-up.service';
+import { UserService } from './user.service';
 
-describe('SignUpService', () => {
-  let service: SignUpService;
+describe('UserService', () => {
+  let service: UserService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(SignUpService);
+    service = TestBed.inject(UserService);
   });
 
   it('should be created', () => {
