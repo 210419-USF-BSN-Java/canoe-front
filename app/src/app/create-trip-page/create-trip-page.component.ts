5758cf33bfb66e7cd3cc5bcb291a353c4a680304
tom <thomcdixon@gmail.com>
2021-06-01 14:08:58 -0700

    fix string split bug (commit 22c450ed3ad8f0979ac20570c5f90fbc2827f96b)

--- app/src/app/create-trip-page/create-trip-page.component.ts
+++ app/src/app/create-trip-page/create-trip-page.component.ts
@@ -3,67 +3,63 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-create-trip-page',
   templateUrl: './create-trip-page.component.html',
-  styleUrls: ['./create-trip-page.component.css']
+  styleUrls: ['./create-trip-page.component.css'],
 })
-
 export class CreateTripPageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   showElement = false;
   showFlights = false;
   showFood = false;
   showAttractions = false;
   showLodging = false;
-  location:string = '';
- 
+  location: string = '';
 
-  addTrip(){
+  addTrip() {
     this.showElement = !this.showElement;
-   }
-   element: string = '';
-   elements: string[] = ['Add Flight', 'Add Lodging','Add Local Attraction', 'Add Local Eatery' ];
-   splitLocation = location.split([,][1]);
- 
-   city: string = this.splitLocation[0];
-   country: string = this.splitLocation[1];
-  addTripElement(element: string){
-    
-      switch(element){
-        case 'Add Flight': 
-            this.showFlights = true;
-            this.showLodging = false; 
-            this.showAttractions = false;
-            this.showFood = false;
-            break;
-        case 'Add Lodging': 
-            this.showFlights = false;
-            this.showLodging = true; 
-            this.showAttractions = false;
-            this.showFood = false;
-            break;
-        case 'Add Local Attraction':  
-            this.showFlights = false;
-            this.showLodging = false; 
-            this.showAttractions = true;
-            this.showFood = false;
-            break;
-        case 'Add Local Eatery': 
-            this.showFlights = false;
-            this.showLodging = false; 
-            this.showAttractions = false;
-            this.showFood = true;
-            break;
-        default: console.log("default"); break;
-      }
-    
-    
   }
+  element: string = '';
+  elements: string[] = [
+    'Add Flight',
+    'Add Lodging',
+    'Add Local Attraction',
+    'Add Local Eatery',
+  ];
+  splitLocation: string[] = [];
 
-
-
-
+  city: string = '';
+  country: string = '';
+  addTripElement(element: string) {
+    switch (element) {
+      case 'Add Flight':
+        this.showFlights = true;
+        this.showLodging = false;
+        this.showAttractions = false;
+        this.showFood = false;
+        break;
+      case 'Add Lodging':
+        this.showFlights = false;
+        this.showLodging = true;
+        this.showAttractions = false;
+        this.showFood = false;
+        break;
+      case 'Add Local Attraction':
+        this.showFlights = false;
+        this.showLodging = false;
+        this.showAttractions = true;
+        this.showFood = false;
+        break;
+      case 'Add Local Eatery':
+        this.showFlights = false;
+        this.showLodging = false;
+        this.showAttractions = false;
+        this.showFood = true;
+        break;
+      default:
+        console.log('default');
+        break;
+    }
+  }
 }
