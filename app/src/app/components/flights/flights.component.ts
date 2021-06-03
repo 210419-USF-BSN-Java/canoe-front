308a17723126592e0fb652a078e7bb6df4352d39
Tom <noxidwebdev@gmail.com>
2021-06-03 09:13:13 -0700

    add flight service and inject destination service. receives destination input via getDestination() (#39)

---
+++ app/src/app/components/flights/flights.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { FlightService } from '../../services/flight.service';
+
+@Component({
+  selector: 'app-flights',
+  templateUrl: './flights.component.html',
+  styleUrls: ['./flights.component.css'],
+})
+export class FlightsComponent implements OnInit {
+  constructor(private fService: FlightService) {}
+
+  ngOnInit(): void {
+    this.fService.test();
+  }
+}
