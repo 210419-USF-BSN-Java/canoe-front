45690239b25e508e62f7a7dba7dc0b55d13f68de
Tom <noxidwebdev@gmail.com>
2021-06-03 20:40:48 -0700

    Feature/from form (#42)
    * add flight service and inject destination service. receives destination input via getDestination()
    
    * add from form and update destination service
    
    * add leaving from form

--- app/src/app/components/from-form/from-form.component.ts
+++ app/src/app/components/from-form/from-form.component.ts
@@ -2,22 +2,22 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { DestinationService } from '../../services/destination.service';
 
 @Component({
-  selector: 'app-destination-form',
-  templateUrl: './destination-form.component.html',
-  styleUrls: ['./destination-form.component.css'],
+  selector: 'app-from-form',
+  templateUrl: './from-form.component.html',
+  styleUrls: ['./from-form.component.css'],
 })
-export class DestinationFormComponent implements OnInit {
-  destination = '';
-
+export class FromFormComponent implements OnInit {
+  from: string = '';
   @Output() controlView = new EventEmitter<string>();
   @Output() formSubmit = new EventEmitter();
+
+  constructor(private dService: DestinationService) {}
+
   submitForm() {
     this.formSubmit.emit(); // sets from in create trip
     this.controlView.emit(); // controls create trip view
-    this.dService.setDestination(this.destination);
+    this.dService.setFrom(this.from);
   }
-
-  constructor(public dService: DestinationService) {}
 
   ngOnInit(): void {}
 }
