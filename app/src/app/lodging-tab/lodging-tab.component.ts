e4b784cb367777d6abfa22bc33062a85d2a86d23
Kevin Cho <kccho2254@gmail.com>
2021-06-04 09:32:44 -0500

    changing post route sign up

--- app/src/app/lodging-tab/lodging-tab.component.ts
+++ app/src/app/lodging-tab/lodging-tab.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-lodging-tab',
   templateUrl: './lodging-tab.component.html',
@@ -11,10 +12,32 @@ import { Component, OnInit } from '@angular/core';
 export class LodgingTabComponent implements OnInit {
 
   lodging: string = '';
+  res: any;
   constructor(private http: HttpClient) { }
   ngOnInit(): void {
   }
+  // onClickSubmit() {
+  //   console.log("Entered lodging: " + this.lodging);
+
+  //   let body = new FormData().append('place', this.lodging);
+  //   const obj = JSON.stringify(body);
+
+  //   const params = new HttpParams().append('place', this.lodging);
+
+  //   const headers = new HttpHeaders().append('Content-Type', 'application/json');
+  //   headers.append('Access-Control-Allow-Origin', '*');
+  //   headers.append('Access-Control-Allow-Methods', "OPTIONS,POST,GET");
+
+
+  //   this.http.post<any>
+  //     ('https://cw2hbv7nwi.execute-api.us-east-2.amazonaws.com/Lodging-API', body,
+  //       {
+  //         headers: headers
+  //       })
+  //     .subscribe((res) => console.log(res));
+  // }
   onClickSubmit() {
+
     console.log("Entered lodging: " + this.lodging);
 
     let body = new FormData().append('place', this.lodging);
@@ -22,15 +45,52 @@ export class LodgingTabComponent implements OnInit {
 
     const params = new HttpParams().append('place', this.lodging);
 
-    const headers = new HttpHeaders().append(
-        'Content-Type',
-        'application/json'
-      );
+    const headers = new HttpHeaders().append('Content-Type', 'application/json');
+    headers.append('Access-Control-Allow-Origin', '*');
+    headers.append('Access-Control-Allow-Methods', "OPTIONS,POST,GET");
 
-    this.http
-      .post<any>('https://cw2hbv7nwi.execute-api.us-east-2.amazonaws.com/Lodging-API', body, {
-        headers: headers
+    let arr: string[] = [];
+
+    this.http.get<any>('https://maps.googleapis.com/maps/api/place/textsearch/json?query=hotels&key=AIzaSyADD_M19PUWlXleB8ix4PnIjLh9F2D90uQ&type=lodging').subscribe(
+      (res) => {
+
+        console.log(res);
+        for (let i = 0; i < res.results.length; i++) {
+          console.log(res.results[i].name);
+          console.log(res.results[i].formatted_address);
+          console.log(res.results[i].rating);
+
+          let data = `
+          <tr>
+              <th scope="row">${i + 1}</th>
+              <td>${res.results[i].name}</td>
+              <td>${res.results[i].formatted_address}</td>
+              <td>${res.results[i].rating}</td>
+          </tr>
+          `
+          arr.push(data);
+        };
+        console.log(arr);
+        let form = document.getElementById('data');
+        if (form) (form as HTMLFormElement);
+
       })
-      .subscribe((res) => console.log(res));
   }
 }
+
+// for (let i = 0; i < res.results.length; i++) {
+//   let data = this.res;
+//   console.log(data + " firing?");
+//   console.log(this.res);
+
+
+//   data = ` 
+//     <tr>
+//         <th scope="row">${i + 1}</th>
+//         <td>${data[i].reimb_submitted}</td>
+//         <td>${data[i].reimb_resolved}</td>
+//         <td>${data[i].reimb_desc}</td>
+
+//     </tr>
+// `;
+// }
--- app/src/app/services/user.service.ts
+++ app/src/app/services/user.service.ts
@@ -31,7 +31,7 @@ export class UserService {
     console.log('***************');
 
     return this.http
-      .post('http://localhost:8085/user/signup', signUpFormData)
+      .post('http://ec2-3-141-202-118.us-east-2.compute.amazonaws.com:8085/user/signup', signUpFormData)
       .pipe(map((res) => res as string));
   }
 
