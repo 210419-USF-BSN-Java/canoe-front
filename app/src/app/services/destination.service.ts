561efeb445398ffb2d47a9b9a187eaf2fbef0f93
Kevin Cho <kccho2254@gmail.com>
2021-06-07 06:12:27 -0500

    destination

--- app/src/app/services/destination.service.ts
+++ app/src/app/services/destination.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Injectable({
@@ -18,14 +19,13 @@ export class DestinationService {
     return this.from;
   }
 
-  setDestination(destination: string) {
+  setDestination(destination: string): Observable<string> {
     this.destination = destination;
+    console.log("posting" + this.destination);
 
       return this.http
         .post('http://3.132.232.218:8085/user/saveDestination', this.destination)
-        .pipe(map((res) => res as string));
-      
-    
+        .pipe(map((res) => res as string));    
   }
 
   getDestination() {
