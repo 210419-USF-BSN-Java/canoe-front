971543b431e6e7d8bd2a0a3795b101f7e0a75599
Kevin Cho <kccho2254@gmail.com>
2021-06-07 04:16:57 -0500

    finishing up css for trip view

--- app/src/app/view-trip-page/view-trip-page.component.ts
+++ app/src/app/view-trip-page/view-trip-page.component.ts
@@ -1,22 +1,22 @@
-import { DataSource } from '@angular/cdk/collections';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { UserService } from '../services/user.service';
 import './app.json';
 
 export interface TripView {
+  num: number;
   destination: string;
   arrivalAirport: string;
   arrivalDate: string;
   departAirport: string;
   departDate: string;
 
-  foodReserveDate: string;
-  foodReservationName: string;
-  tourReserveDate: string;
-  tourReserveName: string;
+  foodReserveDate: string[];
+  foodReservationName: string[];
+  tourReserveDate: string[];
+  tourReserveName: string[];
 
-  checkInDate: string;
+  checkingInDate: string;
   checkOutDate: string;
   hotelName: string;
   hotelRating: string;
@@ -34,8 +34,11 @@ export class ViewTripPageComponent implements OnInit {
 
   block: any = [];
   oneTrip: any[] = [];
+  user = this.uService.getUser();
+
   myTrips: TripView[] = [];
-  displayedColumns: string[] = ['destination', 'arrivalAirport', 'arrivalDate', 'departAirport', 'departDate', 'foodReserveDate', 'foodReservationName', 'tourReserveDate', 'tourReserveName', 'checkinInDate', 'checkOutDate', 'hotelName', 'hotelRating', 'hotel_address', 'pricePerNight'];
+
+  displayedColumns: string[] = ['destination', 'arrivalAirport', 'arrivalDate', 'departAirport', 'departDate', 'foodReserveDate', 'foodReservationName', 'tourReserveDate', 'tourReserveName', 'checkInDate', 'checkOutDate', 'hotelName', 'hotelRating', 'hotel_address', 'pricePerNight'];
   json = [
     {
       "customer": {
@@ -92,11 +95,25 @@ export class ViewTripPageComponent implements OnInit {
           "destinationId": 4,
           "localTouristAttaraction": 3,
           "localTouristAttractionPlace": "San Francisco Zoo"
+        },
+        {
+          "bookedDate": "2021-06-09",
+          "customerId": 3,
+          "destinationId": 4,
+          "localTouristAttaraction": 3,
+          "localTouristAttractionPlace": "Chinatown"
+        },
+        {
+          "bookedDate": "2021-06-08",
+          "customerId": 3,
+          "destinationId": 4,
+          "localTouristAttaraction": 3,
+          "localTouristAttractionPlace": "Redwood Forest"
         }
       ],
       "lodging": {
         "checkInDate": "2021-06-08",
-        "checkOutDate": "2021-06-10",
+        "checkOutDate": "2021-06-28",
         "customerId": 3,
         "destinationId": 4,
         "hotelName": "Courtyard by Marriott San Francisco Fisherman's Wharf",
@@ -158,90 +175,89 @@ export class ViewTripPageComponent implements OnInit {
       ],
       "lodging": {
         "checkInDate": "2021-06-08",
-        "checkOutDate": "2021-06-10",
+        "checkOutDate": "2021-06-30",
         "customerId": 3,
         "destinationId": 4,
-        "hotelName": "Courtyard by Marriott San Jose Fisherman's Wharf",
-        "hotelRating": "4.2",
-        "hotel_address": "580 Beach St, San Jose, CA 94133, United States",
+        "hotelName": "DoubleTree by Hilton Hotel",
+        "hotelRating": "4.7",
+        "hotel_address": "2015 Market Center Blvd, San Jose, CA 94133, United States",
         "lodgingId": 7,
         "pricePerNight": 241
       },
       "lodgingId": 7
     }
   ]
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private uService: UserService) { }
 
   ngOnInit(): void {
+    const user = this.uService.getUser();
+    console.log(user);
+
     this.fillData(this.myTrips);
   }
 
   fillData(data: TripView[]) {
-    let uId;
-    // let username = get the username from the login/signup service;
-
-    // this.http.get<any>(`http://3.132.232.218:8085/user/getUser/${username}`).subscribe(
-    //   (res) => {
-    // uId = res.userId;
+    const uId = this.user.getUserId();
 
     // this.http.get<any>(`http://3.132.232.218:8085/user/gettripplan/${uId}`).subscribe(
     //   (res) => {
 
-    for (let i = 0; i < this.json.length; i++) {
-      this.block.push(i + 1);
-      let dest = this.json[i].destination;
-      let aAirp = this.json[i].flight.arrivalAirport;
-      let arrDate = this.json[i].flight.arrivalDate;
-      let depAir = this.json[i].flight.departAirport;
-      let depDate = this.json[i].flight.departDate;
+        for (let i = 0; i < this.json.length; i++) {
+          this.block.push(i + 1);
+          let dest = this.json[i].destination;
+          let aAirp = this.json[i].flight.arrivalAirport;
+          let arrDate = this.json[i].flight.arrivalDate;
+          let depAir = this.json[i].flight.departAirport;
+          let depDate = this.json[i].flight.departDate;
 
-      let foodResDate = '';
-      let foodResName = '';
-      for (let j = 0; j < this.json[i].localFood.length; j++) {
-        foodResDate = this.json[i].localFood[j].bookedDate;
-        foodResName = this.json[i].localFood[j].locaFoodName;
-      }
+          let foodResDate = [];
+          let foodResName = [];
+          for (let j = 0; j < this.json[i].localFood.length; j++) {
+            foodResDate.push(" " + this.json[i].localFood[j].bookedDate);
+            foodResName.push(" " + this.json[i].localFood[j].locaFoodName);
+          }
 
-      let tourResDate = '';
-      let tourResName = '';
-      for (let k = 0; k < this.json[i].localTouristAttraction.length; k++) {
-        tourResDate = this.json[i].localTouristAttraction[k].bookedDate;
-        tourResName = this.json[i].localTouristAttraction[k].localTouristAttractionPlace;
-      }
+          let tourResDate = [];
+          let tourResName = [];
+          for (let k = 0; k < this.json[i].localTouristAttraction.length; k++) {
+            tourResDate.push(" " + this.json[i].localTouristAttraction[k].bookedDate);
+            tourResName.push(" " + this.json[i].localTouristAttraction[k].localTouristAttractionPlace);
+          }
 
-      let lodgeIn = this.json[i].lodging.checkInDate;
-      let lodgeOut = this.json[i].lodging.checkOutDate;
-      let lodgeNM = this.json[i].lodging.hotelName;
-      let lodgeRat = this.json[i].lodging.hotelRating;
-      let lodgeAdd = this.json[i].lodging.hotel_address;
-      let lodgePri = this.json[i].lodging.pricePerNight;
+          let lodgeIn = this.json[i].lodging.checkInDate;
+          let lodgeOut = this.json[i].lodging.checkOutDate;
+          let lodgeNM = this.json[i].lodging.hotelName;
+          let lodgeRat = this.json[i].lodging.hotelRating;
+          let lodgeAdd = this.json[i].lodging.hotel_address;
+          let lodgePri = this.json[i].lodging.pricePerNight;
 
-      data.push({
-        destination: dest,
-        arrivalAirport: aAirp,
-        arrivalDate: arrDate,
-        departAirport: depAir,
-        departDate: depDate,
+          data.push({
+            num: i+1,
+            destination: dest,
+            arrivalAirport: aAirp,
+            arrivalDate: arrDate,
+            departAirport: depAir,
+            departDate: depDate,
 
-        foodReserveDate: foodResDate,
-        foodReservationName: foodResName,
-        tourReserveDate: tourResDate,
-        tourReserveName: tourResName,
+            foodReserveDate: foodResDate, // more than one is possible
+            foodReservationName: foodResName, // more than one is possible
+            tourReserveDate: tourResDate, // more than one is possible
+            tourReserveName: tourResName, // more than one is possible
 
-        checkInDate: lodgeIn,
-        checkOutDate: lodgeOut,
-        hotelName: lodgeNM,
-        hotelRating: lodgeRat,
-        hotel_address: lodgeAdd,
-        pricePerNight: lodgePri,
+            checkingInDate: lodgeIn,
+            checkOutDate: lodgeOut,
+            hotelName: lodgeNM,
+            hotelRating: lodgeRat,
+            hotel_address: lodgeAdd,
+            pricePerNight: lodgePri,
 
-      })
-    }
+          })
 
-    let arrayDestination:any[] = [];
-
-    this.oneTrip = this.myTrips;
-    console.log(this.myTrips)
-    console.log(this.oneTrip)
+        }
+        this.oneTrip = this.myTrips;
+      // })
   }
 }
+
+
+
